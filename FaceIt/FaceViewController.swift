3d7a9b569993554b98d8842d3b7ff405482deb7c
Guan-Yi <13940863771@163.com>
2016-05-29 17:08:35 +0800

    draw a face

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by YGuan on 5/29/16.
+//  Copyright © 2016 YGuan. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+
+
+}
+
